bfac466f8b9e37ed22a0d228f928d137fb34cc79
Gerhard Unger <gerhard.unger@tugraz.at>
2024-11-06 07:57:15 +0100

    [#175] Add test for cleanup of course deletion

--- tests/diary_entries_lib_test.php
+++ tests/diary_entries_lib_test.php
@@ -157,6 +157,7 @@ final class diary_entries_lib_test extends externallib_advanced_testcase {
      * @throws \restricted_context_exception
      */
     public function test_diary_entry(): void {
+        global $DB;
         $diarydate = new \DateTime('now');
         $diarydate->setTime($diarydate->format('G'), $diarydate->format('i'), 00);
         $diaryenddate = new \DateTime('now');
@@ -223,5 +224,10 @@ final class diary_entries_lib_test extends externallib_advanced_testcase {
         $this::assertEquals(1, $history['Counts'][0]);
 
         external_api::clean_returnvalue(diary_entries_lib::diary_history_returns(), $history);
+
+        // Update 2024-11-05: This test is extended for cleanup (implemented in local_lytix).
+        $this::assertEquals(1, count($DB->get_records('lytix_diary_diary_entries')));
+        delete_course($this->course->id, false);
+        $this::assertEquals(0, count($DB->get_records('lytix_diary_diary_entries')));
     }
 }
